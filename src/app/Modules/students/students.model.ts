5c4b0bc3bd300b5fde3b1af1aa700ff2b414f24b
PmpPiyas <princemahmudpiyas@gmail.com>
2025-07-26 23:33:00 +0600

    Student Interface & Model

---
+++ src/app/Modules/students/students.model.ts
@@ -0,0 +1,99 @@
+import { Schema, Types, model } from "mongoose";
+import {
+  IAuthProvider,
+  IClass,
+  ISection,
+  IStudent,
+} from "./students.interfaces";
+
+const phoneSchema = new Schema<IStudent["phone"]>(
+  {
+    who: { type: String, required: true },
+    number: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const authSchema = new Schema<IAuthProvider>(
+  {
+    provider: { type: String, required: true },
+    providerId: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const studentSchema = new Schema<IStudent>(
+  {
+    name: { type: String, required: true },
+
+    class: {
+      type: String,
+      enum: Object.values(IClass),
+    },
+
+    section: {
+      type: String,
+      enum: Object.values(ISection),
+    },
+
+    phone: {
+      type: [phoneSchema],
+      default: [],
+    },
+
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      sparse: true,
+    },
+
+    fatherName: String,
+    motherName: String,
+
+    password: {
+      type: String,
+      select: false,
+    },
+
+    picture: String,
+    address: String,
+    bloodGroup: String,
+
+    isRemoved: {
+      type: Boolean,
+      default: false,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    auth: {
+      type: [authSchema],
+      default: [],
+    },
+
+    monthlyFee: [
+      {
+        type: Types.ObjectId,
+        ref: "MonthlyFee",
+      },
+    ],
+
+    feedBack: [
+      {
+        type: Types.ObjectId,
+        ref: "TeacherFeedback",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const Student = model<IStudent>("Student", studentSchema);
